27a780bb04f52260c7540b4a4bf78cb833f9cf07
Chuprakov <Acchuprakov@gmail.com>
2026-08-31 15:28:21 +0300

    Prepare project for review

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("ПАУЗА работает");
+"use strict";
+
+const startPauseButton = document.querySelector(".home-start-button");
+const completePauseButton = document.querySelector(".pause-complete-button");
+const savedStartTime = localStorage.getItem("startTime");
+
+if (startPauseButton) {
+    startPauseButton.addEventListener('click', function () {
+        const startTime = Date.now();
+        localStorage.setItem("startTime", startTime);
+        window.location.href = 'pause.html';
+    });
+
+}
+
+if (completePauseButton) {
+    completePauseButton.addEventListener('click', function () {
+        window.location.href = 'completed.html';
+    });
+}
